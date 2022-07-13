360ac8c61dbe3b7f18e412053fa4ad42a623c40b
sathiyajith <sathiyajith19@gmail.com>
2022-07-13 21:53:26 +0530

    Hello world with a button!

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ Prometheus Alerts Simulator and Visualizer
 Java fx or Java Swing or Python Tkinter or Java AWT
 Swing because it is cross platform and has amazing UI. We can choose Java fx also.
 Javafx is latest and can replace Swing.
-Learn Javafx from official doc - https://docs.oracle.com/javafx/2/get_started/hello_world.htm
+##### Learn Javafx from official doc - https://docs.oracle.com/javafx/2/get_started/hello_world.htm
+
+I didnt wait for great ideas to start working on a project. Just wanted to work on something different.
+
+
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,36 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        //Added new button to say hello world!
+        Button hellobutton = new Button();
+        hellobutton.setText("Say Hello World");
+        hellobutton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello world");
+            }
+        });
+
+        StackPane rootPane = new StackPane();
+        rootPane.getChildren().add(hellobutton);
+        primaryStage.setScene(new Scene(rootPane, 300, 275));
         primaryStage.show();
     }
 
